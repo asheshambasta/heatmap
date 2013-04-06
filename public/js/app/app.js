d79b08987dbe01fc12665844384313d7f0d34d88
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2013-04-06 19:08:01 +0530

    Ok, this looks ok for now, off to more work

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,5 +1,37 @@
+var userID        = null;
+var userCompany   = null;
+var userName      = null;
+var accessToken   = null;
+
+/**
+ * Set the user id from API
+ * @param   String  accessToken The access token for API calls
+ */
+function setUserID(accessToken) {
+  //make ajax call to API
+  $.ajax({
+    url: 'http://api.engagor.com/me',
+    data: {access_token: accessToken},
+    type: 'post',
+    success: function(response) {
+      console.log(response);
+      if(200 == response.meta.code) {
+        userID        = response.response.id;
+        userCompany   = response.response.company;
+        userName      = response.response.name;
+      }
+    },
+    dataType: 'jsonp'
+  });
+}
+
+
+/**
+ * All the stuff happens here
+ */
 $(document).ready(
   function() {
-  console.log("document ready, begin some cool stuff");
-}
-);
+  //set the access token and the user id
+  accessToken = $("#access_token").data('access_token');
+  setUserID(accessToken);
+});
