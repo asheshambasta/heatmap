07a35eee98241047617c8497edb9c9649e529b2d
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2013-04-07 10:00:50 +0530

    Renaming app.js to heatmap.js

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['access_token'])) {
 <body>
   <script src="js/std/d3.v3.min.js"></script>
   <script src="js/std/jquery-1.9.1.min.js"></script>
-  <script src="js/app/app.js"></script>
+  <script src="js/app/heatmap.js"></script>
   <script src="js/app/ready.js"></script>
   <div id="access_token" data-access_token="<?php echo $_SESSION['access_token']; ?>"></div>
 <div id="chart_container">
