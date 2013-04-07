3e5542f41fb66caea74a82bff7c8712b86b7bc6f
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2013-04-07 09:45:08 +0530

    Cleaned up a little bit, colour function inside app class working ok

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,101 +1,112 @@
-var userID        = null;
-var userCompany   = null;
-var userName      = null;
-var userEmail     = null;
-var accessToken   = null;
-var sumCells      = 0;
-var allCellValues = [];
+var app = {
+  userID: null,
+  userCompany: null,
+  userName: null,
+  userEmail: null,
+  accessToken: null,
+  sumCells: 0,
+  allCellValues: [],
+  maxCellValue: 0,
 
-/**
- * Set the user id from API
- * @param   String  accessToken The access token for API calls
- */
-function setUserID(accessToken) {
-  //make ajax call to API
-  $.ajax({
-    url: 'http://api.engagor.com/me',
-    data: {access_token: accessToken},
-    type: 'post',
-    success: function(response) {
-      console.log(response);
-      if(200 == response.meta.code) {
-        userID        = response.response.id;
-        userCompany   = response.response.company;
-        userName      = response.response.name;
-        userEmail     = response.response.email;
-      }
-    },
-    dataType: 'jsonp'
-  });
-}
-
-function addRow(cellValues) {
-
-  var container = d3.select('div#chart_container');
-  var rows = container.selectAll('div.row');
-  var newRowID = "row_" + rows[0].length;
-  container.append('div').classed('row', true).attr('id', newRowID);
-
-  //add cells to the new row
-  var row = container.select('div#' + newRowID);
-  for(var i = 0; i < cellValues.length; i++) {
-    sumCells += cellValues[i];
-    allCellValues.push(cellValues[i]);
-    var cellID = "cell_" + newRowID + "_" + i;
-    row.append('div').classed('cell_white', true).attr('id', cellID);
-  }
-  row.selectAll('div').data(cellValues).attr(
-    'title', 
-    function(cellValue) {
-      return cellValue;
+  /**
+   * Set the user id from API
+   * @param   String  accessToken The access token for API calls
+   */
+  setUserID: function(accessToken) {
+    //make ajax call to API
+    var that = this;
+    $.ajax({
+      url: 'http://api.engagor.com/me',
+      data: {access_token: accessToken},
+      type: 'post',
+      success: function(response) {
+        console.log(response);
+        if(200 == response.meta.code) {
+          that.userID        = response.response.id;
+          that.userCompany   = response.response.company;
+          that.userName      = response.response.name;
+          that.userEmail     = response.response.email;
+        }
+      },
+      dataType: 'jsonp'
     });
-}
+  },
 
-function paintGrid() {
-  var container = d3.select('div#chart_container');
-  container
+  addRow: function(cellValues) {
+
+    var container = d3.select('div#chart_container');
+    var rows = container.selectAll('div.row');
+    var newRowID = "row_" + rows[0].length;
+    container.append('div').classed('row', true).attr('id', newRowID);
+
+    //add cells to the new row
+    var row = container.select('div#' + newRowID);
+    for(var i = 0; i < cellValues.length; i++) {
+      this.sumCells += cellValues[i];
+      this.allCellValues.push(cellValues[i]);
+      this.maxCellValue = Math.max(this.maxCellValue, cellValues[i]);
+      var cellID = "cell_" + newRowID + "_" + i;
+      row.append('div').classed('cell_white', true).attr('id', cellID);
+    }
+    row.selectAll('div').data(cellValues).attr(
+      'title', 
+      function(cellValue) {
+        return cellValue;
+      });
+  },
+
+  paintGrid: function() {
+    var container = d3.select('div#chart_container');
+    var that = this;
+    container
     .selectAll('div.cell_white')
-    .data(allCellValues)
+    .data(this.allCellValues)
     .style(
       'background-color', 
       function(cellValue) { 
-        return getColor(cellValue, sumCells); 
+        var colour = that.getColour(cellValue, that.sumCells);
+        console.log(colour);
+        return colour; 
       });
-}
+  },
 
-function getColor(cellValue, sumCells) {
-  var R, G, B;
-  R = G = B = 255;
-  var frac = cellValue/(sumCells ? sumCells : 1);
-  var diff = Math.round(255 * frac);
-  B -= diff;
-  G -= diff;
-  return 'rgb(' + R + ',' + G + ',' + B + ')';
-}
+  getColour: function(cellValue, sumCells) {
+    var R, G, B, A;
+    R = 255;
+    //see how close it is to the max cell value
+    var diffFromMax = this.maxCellValue - cellValue;
+    //the smaller diffFromMax, the redder it should be.
+    var diffFrac = diffFromMax/(this.maxCellValue ? this.maxCellValue : 1);
+    B = Math.round(255 * diffFrac);
+    G = Math.round(100 * diffFrac);
+    A = 0.3 + 0.7 * (cellValue/this.maxCellValue);
+    return 'rgba(' + R + ',' + G + ',' + B + ',' + A + ')';
+  },
 
-function getInsights(facetDefs, dateFrom, dateTo) {
-  if(!userID) {
-    return false;
+  getInsights: function(facetDefs, dateFrom, dateTo) {
+    if(!userID) {
+      return false;
+    }
+    var url = 'http://api.engagor.com/' + userID +  '/insights/facets';
+    console.log("Trying to get insights from: " + url);
+    var data = {
+      facetdefinitions: JSON.stringify(facetDefs),
+      date_from: dateFrom,
+      date_to: dateTo,
+      access_token: accessToken
+    };
+    console.log("Using data: ");
+    console.log(data);
+    $.ajax({
+      url: url,
+      data: data,
+      success: function(response) {
+        console.log(response);
+      },
+      type: 'POST',
+      dataType: 'jsonp'
+    });
   }
-  var url = 'http://api.engagor.com/' + userID +  '/insights/facets';
-  console.log("Trying to get insights from: " + url);
-  var data = {
-    facetdefinitions: JSON.stringify(facetDefs),
-    date_from: dateFrom,
-    date_to: dateTo,
-    access_token: accessToken
-  };
-  console.log("Using data: ");
-  console.log(data);
-  $.ajax({
-    url: url,
-    data: data,
-    success: function(response) {
-      console.log(response);
-    },
-    type: 'POST',
-    dataType: 'jsonp'
-  });
 }
 
 
@@ -105,6 +116,6 @@ function getInsights(facetDefs, dateFrom, dateTo) {
 $(document).ready(
   function() {
   //set the access token and the user id
-  accessToken = $("#access_token").data('access_token');
-  setUserID(accessToken);
+  app.accessToken = $("#access_token").data('access_token');
+  app.setUserID(app.accessToken);
 });
